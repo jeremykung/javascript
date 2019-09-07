988bd04723486f605831e7d3965159333c3a1079
jeremykung <jeremykung@gmail.com>
2019-09-07 09:50:38 +0800

    starting coding challenge 1

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var name = 'Jeremy';
-console.log("Hi " + name);
+// var name = 'Jeremy';
+// console.log("Hi " + name);
 
-var ageSafe = true;
-console.log (ageSafe);
+// var ageSafe = true;
+// console.log (ageSafe);
 
-// alert(name);
+// // alert(name);
 
-var yourName = prompt('What\'s your name?');
+// var yourName = prompt('What\'s your name?');
 
-alert(yourName);
+// alert(yourName);
+
+// Coding Challenge 1 BMI
+
+var markHeight, markWeight, johnHeight, johnWeight;
+
+markHeight = 180;
+
+console.log ("mark's height is " + markHeight);
