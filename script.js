51a92c5e41142391c749b907d3a5b0ebb593b353
jeremykung <jeremykung@gmail.com>
2019-09-07 09:02:48 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = 'Jeremy';
+console.log("Hi " + name);
+
+var ageSafe = true;
+console.log (ageSafe);
+
+// alert(name);
+
+var yourName = prompt('What\'s your name?');
+
+alert(yourName);
