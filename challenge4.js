531068806194fb25ccae4e5e71f0a82c4a65328b
Jeremy Kung <jeremykung@gmail.com>
2019-09-30 10:30:37 +0800

    updated challenge 4

--- challenge4.js
+++ challenge4.js
@@ -3,8 +3,8 @@
 
 var john = {
     name: 'John',
-    mass: 60,
-    height: 170,
+    mass: 70,
+    height: 180,
     calcBMI: function(mass, height) {
         this.bmi = this.mass / (this.height/100)**2;
     }
@@ -23,5 +23,21 @@ john.calcBMI();
 document.write("John's BMI: " + john.bmi + "<br>");
 
 mike.calculateBMI();
-document.write("Mike's BMI: " + mike.bmi);
+document.write("Mike's BMI: " + mike.bmi + "<br>");
 
+if (mike.bmi > john.bmi) {
+    document.write("Mike's BMI is higher than John's");
+} else if (john.bmi > mike.bmi) {
+    document.write("John's BMI is higher than Mike's BMI");
+} else {
+    document.write("Their BMI's are the same");
+}
+
+// You can methods in the IF/ELSE statements as well
+if (mike.calculateBMI() > john.calcBMI()) {
+    document.write("Mike's BMI is higher than John's");
+} else if (john.calcBMI() > mike.calculateBMI()) {
+    document.write("John's BMI is higher than Mike's BMI");
+} else {
+    document.write("Their BMI's are the same");
+}
