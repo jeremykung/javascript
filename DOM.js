e4ee011bfb37abca726f3fcc14ada069b0e8b36d
Jeremy Kung <jeremykung@gmail.com>
2019-10-12 14:44:33 +0800

    finished pig game

--- DOM.js
+++ DOM.js
@@ -1,11 +1,23 @@
-var scores, roundScores, activePlayer;
+var scores, roundScores, activePlayer, nameP0, nameP1;
 
 scores = [0,0];
 roundScore = 0; // Counts round score
 activePlayer = 0;
 
 
-// document.querySelector(".dicePic").style.display = "none";
+// CUSTOM NAMES
+
+// nameP0 = prompt("What's your name Player 1?");
+// nameP1 = prompt("What's your name Player 2?");
+// console.log(nameP0);
+// console.log(nameP1);
+
+// document.querySelector(".p0-name").textContent = nameP0;
+// document.querySelector(".p1-name").textContent = nameP1;
+
+
+
+// Event for ROLL
 
 document.querySelector(".roll").addEventListener('click', function() {
     // Anonmyous Function
@@ -23,16 +35,29 @@ document.querySelector(".roll").addEventListener('click', function() {
     // 3. Add to round score
     var round = document.querySelector(".p" + activePlayer + "-round");
 
-
+    // 4. Check roll
     if (dice === 1) {
+   
         roundScore = 0;
+        round.textContent = roundScore;
+
+        if (activePlayer === 0) {
+            activePlayer += 1;
+        } else {
+            activePlayer -= 1;
+        };
+
     } else {
         roundScore += dice;
     }
 
     round.textContent = roundScore;
 
+    console.log("player: " + activePlayer);
+
 });
+
+// Event for HOLD
 
 document.querySelector(".hold").addEventListener("click", function() {
     
@@ -41,8 +66,79 @@ document.querySelector(".hold").addEventListener("click", function() {
 
     document.querySelector(".p" + activePlayer + "-total").textContent = scores[activePlayer];
 
-    return;
-})
+    var round = document.querySelector(".p" + activePlayer + "-round");
+
+    roundScore = 0;
+    round.textContent = roundScore;
+
+    if (activePlayer === 0) {
+        activePlayer += 1;
+    } else {
+        activePlayer -= 1;
+    };
+
+    // Win Condition
+
+    if (scores[0] >= 100) {
+
+        console.log("GameOver");
+        document.querySelector(".play-game").style.display = "none";
+
+        document.querySelector(".game-over").style.display = "block";
+
+        document.querySelector(".win").textContent = "P1 Wins";
+
+        // CUSTOM NAMES
+        // document.querySelector(".win").textContent = nameP0 + " Wins";
+
+    } else if (scores[1] >= 100) {
+
+        console.log("GameOver");
+        document.querySelector(".play-game").style.display = "none";
+
+        document.querySelector(".game-over").style.display = "block";
+
+        document.querySelector(".win").textContent = "P2 Wins";
+
+        // CUSTOM NAMES
+        // document.querySelector(".win").textContent = nameP1 + " Wins";
+
+    };
+
+    console.log("player: " + activePlayer);
+
+});
+
+// RESET Game
+
+document.querySelector(".reset").addEventListener("click", function() {
+    scores = [0,0];
+
+    document.querySelector(".game-over").style.display = "none";
+
+    document.querySelector(".play-game").style.display = "block";
+
+    document.querySelector(".p0-total").textContent = 0;
+    document.querySelector(".p0-round").textContent = 0;
+    document.querySelector(".p1-total").textContent = 0;
+    document.querySelector(".p1-round").textContent = 0;
+
+});
+
+// switch (true) {
+//     case (scores[0] >= 20):
+//         console.log("Game over");
+//         document.querySelector(".game-over").style.display = "block";
+//         break;
+    
+//     case (scores[1] >= 20):
+//         document.querySelector(".game-over").style.display = "block";
+//         break;
+
+//     default:
+//         break;
+// };
+
 
 
 
