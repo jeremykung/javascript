706cfa46159960aca468cee72bff22abf478f524
Jeremy Kung <jeremykung@gmail.com>
2020-02-08 20:55:23 +0800

    rearranged entire folder

--- apps/budget2.js
+++ apps/budget2.js
@@ -1,13 +1,45 @@
+var hear = 3;
+
+// Below is an IIFE (Immediately Invoked Function Expression)
+// ONLY the returned code is PUBLIC, everything else is PRIVATE
+
 var budgetController = (function(){
     var x = 23;
-    var add = function(a) {
+    var add = function(a) { // This function is gone after execution (therefore private)
         return x+a;
     }
 
-    return {
+    return { // INNER FUNCTION 
+        // has access to outer function because of CLOSURES
+        // therefore, it can access the add function outside of it
+        // it has CLOSED in and collected the outer functions 
         publicTest: function(b){
-            console.log(add(2));
-        };
+            console.log(add(b));
+        }
     }
+})();
+
+
+
+
+
+var UIController = (function() {
+
+    // Code
 
 })();
+
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
